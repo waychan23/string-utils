32debe7b2a4f6b693c94d5fec462989bfa762c60
WayChan <waychan23@hotmail.com>
2017-08-15 20:22:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 "use strict";
 
-/* /(([A-Z]{1}|[A-Z][a-z]+|[0-9]+))/g */
-/* /(([A-Z]{1}|[A-Z][a-z0-9]+))/g */
-
 module.exports.camel2underscore = function(str, cap, numberAlone){
 	//对不正确的驼峰式先纠正, 例如数字后面直接跟小写字母，应该更为大写
 	str = (str || '').replace(/[0-9][a-z]/g, g => g.replace(/[a-z]$/, l => l.toUpperCase()));
